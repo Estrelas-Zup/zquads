b823f60da50bb833c7e4442f00cfa9b3fde49dcd
joaoalveszup <joao.alves@zup.com.br>
2020-11-20 15:58:03 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/estrelas/zquads/ZquadsApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.zquads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZquadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZquadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/estrelas/zquads/ZquadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.zquads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZquadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
