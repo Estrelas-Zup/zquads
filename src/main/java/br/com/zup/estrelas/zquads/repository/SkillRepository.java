fbc18797f9573811c9dcff1189ad0cb01530fb9b
Gustavo Andrade <gustavoandrade15@hotmail.com>
2020-11-27 13:51:13 -0300

    Crud skill

--- src/main/java/br/com/zup/estrelas/zquads/repository/SkillRepository.java
+++ src/main/java/br/com/zup/estrelas/zquads/repository/SkillRepository.java
@@ -1,10 +1,13 @@
 package br.com.zup.estrelas.zquads.repository;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import br.com.zup.estrelas.zquads.domain.Skill;
+import br.com.zup.estrelas.zquads.enums.SkillType;
 
 @Repository
 public interface SkillRepository extends CrudRepository<Skill, Long> {
 
+    List<Skill> findByType(SkillType type);
 }
