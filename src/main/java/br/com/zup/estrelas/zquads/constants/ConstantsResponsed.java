3d22df25159f8103bc455f7f1054c4b7959bfdd0
Drigo Zup <rodrigo.lima@zup.com.br>
2020-12-19 17:58:33 -0300

    ajustes pontuais

--- src/main/java/br/com/zup/estrelas/zquads/constants/ConstantsResponsed.java
+++ src/main/java/br/com/zup/estrelas/zquads/constants/ConstantsResponsed.java
@@ -4,26 +4,26 @@ public class ConstantsResponsed {
 
     //Users
     
-    public static final String USER_ALREADY_PRESENT = "THIS USER ALREADY EXISTS";
-    public static final String USER_NOT_FOUND = "THIS USER DOES NOT EXIST";
-    public static final String SUCCESSFULLY_DELETED = "THIS USER WAS DELETED";
-    public static final String SKILL_ALREADY_PRESENT = "THIS SKILL ALREADY EXISTS";
-    public static final String SKILL_NOT_FOUND = "THIS SKILL DOES NOT EXIST";
+    public static final String USER_ALREADY_PRESENT = "USER ALREADY EXISTS";
+    public static final String USER_NOT_FOUND = "USER NOT FOUNF";
+    public static final String SUCCESSFULLY_DELETED = "USER WAS SUCCESSFULLY DELETED";
+    public static final String SKILL_ALREADY_PRESENT = "SKILL ALREADY EXISTS";
+    public static final String SKILL_NOT_FOUND = "SKILL NOT FOUND";
     
     //Squads
     
-    public static final String SQUAD_NOT_FOUND = "Squad does not exist!";
-    public static final String SQUAD_REMOVED = "Squad removed";
-    public static final String SQUAD_ALREADY_PRESENT = "Existing squad";
-    public static final String PROJECT_FINISHED = "Project successfully finished";
+    public static final String SQUAD_NOT_FOUND = "SQUAD NOT FOUND!";
+    public static final String SQUAD_REMOVED = "SQUAD WAS SUCCESSFULLY DELETED";
+    public static final String SQUAD_ALREADY_PRESENT = "SQAUD ALREADY EXISTS";
+    public static final String PROJECT_FINISHED = "PROJECT SUCCESSFULLY FINISHED";
     
     //Tasks
     
-    public static final String TASK_SUCCESSFULLY_DELETED = "task successfully deleted";
-    public static final String TASK_SUCCESSFULLY_FINISHED = "task successfully finished";
-    public static final String TASK_NOT_FOUND = "Task Not Found";
+    public static final String TASK_SUCCESSFULLY_DELETED = "TASK WAS SUCCESSFULLY DELETED";
+    public static final String TASK_SUCCESSFULLY_FINISHED = "TASK WAS SUCCESSFULLY FINISHED";
+    public static final String TASK_NOT_FOUND = "TASK NOT FOUND";
     
     //Feed Element
-    public static final String FEED_ELEMENT_DELETE_SUCESSFULLY = "Feed Element was sucessfully deleted.";
-    public static final String FEED_ELEMENT_NOT_FOUND = "This feed element not found.";
+    public static final String FEED_ELEMENT_DELETE_SUCESSFULLY = "COMENTARY WAS SUCCESSFULLY DELETED";
+    public static final String FEED_ELEMENT_NOT_FOUND = "COMENTARY NOT FOUND";
 }
